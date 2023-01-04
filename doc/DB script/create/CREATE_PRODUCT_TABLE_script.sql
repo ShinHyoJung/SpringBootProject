3124ae545aea0ef0b35be54fe806e5c5718b9526
ShinHyoJung <59465978+ShinHyoJung@users.noreply.github.com>
2023-01-04 22:18:27 +0900

    2023-01-04 : product 테이블 생성쿼리 수정

--- doc/DB script/create/CREATE_PRODUCT_TABLE_script.sql
+++ doc/DB script/create/CREATE_PRODUCT_TABLE_script.sql
@@ -2,10 +2,10 @@ CREATE TABLE product (
      product_id int not null auto_increment,
      code varchar(200) not null,
      name varchar(200) not null,
-     price varchar(200) not null,
      full_quantity int,
      sold_quantity int,
      left_quantity int,
+     image varchar(300),
      register_date datetime,
      update_date datetime,
      primary key(product_id)
